90e6aa529ed3b4e676c5e121ade03f9380f8d447
张锋 <zhangfeng930101@qq.com>
2018-07-02 09:14:15 +0800

    child-ref

--- src/views/file/reader.vue
+++ src/views/file/reader.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
-    <span>file-reader</span>
+    <span>the refs of child test-reader</span>
   </div>
 </template>
+<script>
+export default {
+  methods: {
+    handleConsoleRef: function () {
+      console.log('ref is successful of child')
+    }
+  }
+}
+</script>
+
 <style>
 
 </style>
