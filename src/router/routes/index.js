b1818a43b52102e4b7ef3cdd1949547f9cbe39fd
张锋 <zhangfeng930101@qq.com>
2018-10-09 09:29:38 +0800

    s

--- src/router/routes/index.js
+++ src/router/routes/index.js
@@ -1,9 +1,17 @@
 import { nujeyDemos } from './nujey-demos.js'
 // const _import = require('./import')
-const _import = function(file) {
-  return () => import(file + '.vue')
+const _import = (file) => {
+  return () => import(`../../views/${file}.vue`)
 }
-console.log(_import())
+
+const _import_one = (file) => {
+  return (resolve) => require([`../../views/${file}.vue`], resolve)
+}
+
+// const _import_two = (file, groupName) => {
+//   return r => require.ensure([], r(require(`../../views/${file}.vue`)), groupName)
+// }
+
 export default [
   {
     path: '/',
@@ -11,8 +19,7 @@ export default [
       layout: true,
       title: '首页'
     },
-    // component: () => import('../../views/file/index.vue'),
-    component: _import('../../views/file/index'),
+    component: _import('file/index'),
     children: [{
       name: 'raber',
       path: 'raber',
@@ -28,7 +35,8 @@ export default [
         title: 'filereader',
         layout: true
       },
-      component: (resolve) => require(['../../views/file/reader.vue'], resolve)
+      // component: (resolve) => require(['../../views/file/reader.vue'], resolve)
+      component: _import_one('file/reader')
     }, {
       name: 'sass',
       path: 'sass',
@@ -45,7 +53,8 @@ export default [
         title: 'test-store',
         layout: true
       },
-      component: () => import('../../views/file/test-store.vue')
+      // component: () => import('../../views/file/test-store.vue')
+      component: _import_one('file/test-store')
     }]
   },
   ...nujeyDemos
