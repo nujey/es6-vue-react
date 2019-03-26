5e530af7a7e4ec383e68bdb4f283dc5731415f69
张锋 <zhangfeng930101@qq.com>
2019-03-26 09:05:07 +0800

    slot

---
+++ src/views/index/components/test-render.vue
@@ -0,0 +1,16 @@
+
+<script>
+  import Vue from 'vue'
+  Vue.component('test-render', {
+    template: '#test-render',
+    render(createElement) {
+      return createElement(
+        'h' + this.level,
+        this.level
+      )
+    },
+    props: {
+      level: { type: Number, required: true }
+    }
+  })
+</script>
---
+++ src/views/index/components/test-slot.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>111</div>
+    <slot>我是一个slot</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
