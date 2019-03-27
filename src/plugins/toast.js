8803703c038e44d84df507048d4c4e055e738a57
张锋 <zhangfeng930101@qq.com>
2019-03-27 14:38:46 +0800

    toast插件的简易版本

--- src/plugins/toast.js
+++ src/plugins/toast.js
@@ -1,7 +1,32 @@
 var Toast = {}
 
-Toast.install = function(Vue, options) {
+Toast.install = function(Vue, options = {}) {
   // Vue.prototype.$msg = 'Hello World'
+  options.duration = 2000
+  options.defaultType = 'bottom'
+
+  Vue.prototype.$toast = (tips, type) => {
+    if (type) {
+      options.defaultType = type
+    }
+    if (document.getElementsByClassName('vue-toast').length) return
+
+    let toastTpl = Vue.extend({
+      template: '<div class="vue-toast toast-' + type + '">'+ tips +'</div>'
+    })
+    let tpl = new toastTpl().$mount().$el;
+    // console.log(tpl.offsetWidth, tpl.clientWidth)
+    // tpl.style.marginLeft = - 100 / 2+ 'px'
+    document.body.appendChild(tpl)
+
+    setTimeout(function() {
+      document.body.removeChild(tpl)
+    }, 2000)
+  }
+  // 用来展示在不同的位置
+  ['bottom', 'top', 'center'].forEach(type => {
+    Vue.prototype.$toast[type] = tips => Vue.prototype.$toast(tips, type)
+  })
 }
 
 module.exports = Toast
