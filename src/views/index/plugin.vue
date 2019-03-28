162ed5a14e2d54a4730bf0b8c407344a43af0baa
张锋 <zhangfeng930101@qq.com>
2019-03-28 16:45:10 +0800

    toast plugin

--- src/views/index/plugin.vue
+++ src/views/index/plugin.vue
@@ -6,12 +6,29 @@
 import Vue from 'vue'
 
 export default {
+  data() {
+    return {
+      arr1: [{id: 1, age: 20}],
+      arr2: [{id: 2, age: 21}]
+    }
+  },
   created() {
-    console.log(Vue.pluginGlobalProps)
+    console.log([...this.arr1, ...this.arr2])
+
+    let params = {
+      name: '张小鹿',
+      title: 20,
+      type: 'type1'
+    }
+    const { type, ...other } = params
+    console.log(type, ...other)
   },
   mounted() {
     // console.log(this.$firstPlugin())
-    this.$toast('我', 'center')
+    this.$toast('我', {
+      type: 'center',
+      duration: 3000
+    })
   }
 }
 </script>
