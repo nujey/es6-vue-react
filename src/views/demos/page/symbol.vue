41ea71d935096406b254447400f16d624751b516
张锋 <zhangfeng930101@qq.com>
2019-04-01 16:47:15 +0800

    属性名symbol

--- src/views/demos/page/symbol.vue
+++ src/views/demos/page/symbol.vue
@@ -20,7 +20,38 @@ export default {
       }
     }
     let objSym = Symbol(obj)
-    console.log(objSym)
+    // console.log(objSym) // abc
+
+    let mySymbol = Symbol()
+
+    let a = {
+      [mySymbol]: ((i) => {return i + 'KKK'})(0)
+    }
+
+    Object.defineProperty(a, Symbol(), { value: 'PHP' })
+
+    a[Symbol('a')] = 'Hello'
+    a[Symbol('b')] = 'world'
+    console.log(a)
+
+    const COLOR_RED = Symbol()
+    const COLOR_GREEN = Symbol()
+
+    function testSymbol(color) {
+      switch(color) {
+        case COLOR_RED:
+          console.log(COLOR_RED, 'COLOR_RED')
+          return COLOR_RED;
+        case COLOR_GREEN:
+          console.log(COLOR_GREEN, 'COLOR_GREEN')
+          return COLOR_GREEN;
+        default:
+          throw new Error('undefined color')
+      }
+    }
+
+    testSymbol(COLOR_GREEN)
+    
   }
 }
 </script>
