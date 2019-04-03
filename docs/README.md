8559224a43f82afa9b4f2a673554986346be33f0
张锋 <zhangfeng930101@qq.com>
2019-04-03 15:37:50 +0800

    docsify使用测试

---
+++ docs/README.md
@@ -0,0 +1,10 @@
+
+# Vue相关练习
+### 围绕原始数据类型创建一个显式包装器对象从 ECMAScript 6 开始不再被支持。 然而，现有的原始包装器对象，如 new Boolean、new String以及new Number因为遗留原因仍可被创建。
+
+<!-- [a1adqwf](https://www.cc.com) -->
+
+# 目录
+* [导航1]()
+* [导航2]()
+* [导航3]()
--- src/views/demos/page/object.vue
+++ src/views/demos/page/object.vue
@@ -11,10 +11,10 @@ export default {
     const b = {
       b: 2
     }
-    console.log(Object.length)
+    // console.log(Object.length)
     const c = Object.assign({}, a, b)
-    console.log(Object.entries(c))
-    console.log(Object.seal(a))
+    // console.log(Object.entries(c))
+    // console.log(Object.seal(a))
 
     var obj = {}
     Object.defineProperties(obj, {
@@ -27,8 +27,42 @@ export default {
         value: 'bbb'
       }
     })
+    
     // obj.b = 'bbbbbbb'
-    console.log(obj)
+    Object.defineProperty(obj, 'a', {
+      enumerable: false, // 置为true的时候，才能出现在枚举属性中
+      configurable: false, // 该属性才能被改变，同时该属性能在对象上被删除
+      writable: true,
+      value: '测试'
+    })
+    Object.freeze(obj)
+    // console.log(obj)
+    Object.isFrozen(obj)
+
+    let sealObj = { a: '1' }
+    Object.seal(sealObj)
+    sealObj.a = '2'
+    // Object.defineProperty(obj, 'b', {
+    //   writable: true,
+    //   value: '测试'
+    // })
+    // delete sealObj.a
+    // console.log(sealObj, 'aaaa')
+
+    let getObjs = { a: 1, b: 2, c: 3}
+    getObjs[Symbol('foo')] = 'symbol'
+    console.log(getObjs)
+    const arr = Object.getOwnPropertyNames(getObjs)
+    console.log(arr)
+    const arr1 = Object.getOwnPropertySymbols(getObjs)
+    console.log(arr1)
+    const d1 = Object.getOwnPropertyDescriptors(getObjs, "a")
+    console.log(d1)
+
+    var objEn = { a: 'a1', b: 'b1', c: 'c1' }
+    console.log(Object.entries(objEn)) 
+    const res = Object.fromEntries(Object.entries(objEn))
+    console.log(res)
   }
 }
 </script>
