257025ec5a4e25a8ca7ba7d1d7df55bc159ff495
张锋 <zhangfeng930101@qq.com>
2019-04-08 16:30:53 +0800

    loading

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
+import loadingself from './loading/index'
 import MyFirstPlugin from './first-plugin'
 import Toast from './toast.js'
 
 Vue.use(MyFirstPlugin)
 Vue.use(Toast)
+
+Vue.use(loadingself)
--- src/views/file/reader.vue
+++ src/views/file/reader.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     <span class="scoped_child">the refs of child test-reader</span>
-    <send-msg></send-msg>
+    <!-- <send-msg></send-msg> -->
   </div>
 </template>
 <script type="text/bable">
 export default {
+  created() {
+    this.$loadingself('加载中')
+  },
   methods: {
     handleConsoleRef: function () {
       console.log('ref is successful of child')
