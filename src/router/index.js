fa1d3b3f640c8542092f91b4ff3ae1eaefa0254e
张锋 <zhangfeng930101@qq.com>
2019-05-21 17:08:10 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import routes from './routes'
+import about from '../views/router/index.vue'
 
 Vue.use(Router)
 const router = new Router({
-  routes,
+  // routes,
+  routes: [
+    {
+      path: '/router-module',
+      component: about
+    }
+  ],
   mode: 'history'
 })
 
